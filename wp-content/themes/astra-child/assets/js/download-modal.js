e1b35e4cc8469f7abafe68a4128ebddfa06ed285
popcorn0118 <popcorn0118@gmail.com>
2026-07-07 17:44:18 +0800

    拿掉 child 命名字眼

--- wp-content/themes/astra-child/assets/js/download-modal.js
+++ wp-content/themes/astra-child/assets/js/download-modal.js
@@ -3,19 +3,19 @@
 
 	document.addEventListener('DOMContentLoaded', function () {
 
-		var wrappers = document.querySelectorAll('.child-download');
+		var wrappers = document.querySelectorAll('.download');
 
 		wrappers.forEach(function (wrapper) {
 
 			var objectId  = wrapper.getAttribute('data-object-id');
-			var trigger   = wrapper.querySelector('.child-download__trigger');
-			var modal     = wrapper.querySelector('.child-download-modal');
-			var overlay   = wrapper.querySelector('.child-download-modal__overlay');
-			var closeBtn  = wrapper.querySelector('.child-download-modal__close');
-			var form      = wrapper.querySelector('.child-download-modal__form');
-			var input     = wrapper.querySelector('.child-download-modal__input');
-			var errorMsg  = wrapper.querySelector('.child-download-modal__error');
-			var submitBtn = wrapper.querySelector('.child-download-modal__submit');
+			var trigger   = wrapper.querySelector('.download__trigger');
+			var modal     = wrapper.querySelector('.download-modal');
+			var overlay   = wrapper.querySelector('.download-modal__overlay');
+			var closeBtn  = wrapper.querySelector('.download-modal__close');
+			var form      = wrapper.querySelector('.download-modal__form');
+			var input     = wrapper.querySelector('.download-modal__input');
+			var errorMsg  = wrapper.querySelector('.download-modal__error');
+			var submitBtn = wrapper.querySelector('.download-modal__submit');
 
 			if (!trigger || !modal) {
 				return;
@@ -24,7 +24,7 @@
 			function openModal() {
 				modal.classList.add('is-active');
 				modal.setAttribute('aria-hidden', 'false');
-				document.body.classList.add('child-download-modal-open');
+				document.body.classList.add('download-modal-open');
 
 				if (errorMsg) {
 					errorMsg.hidden = true;
@@ -39,7 +39,7 @@
 			function closeModal() {
 				modal.classList.remove('is-active');
 				modal.setAttribute('aria-hidden', 'true');
-				document.body.classList.remove('child-download-modal-open');
+				document.body.classList.remove('download-modal-open');
 			}
 
 			trigger.addEventListener('click', openModal);
--- wp-content/themes/astra-child/inc/download-block.php
+++ wp-content/themes/astra-child/inc/download-block.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @return string|int|false
  */
-function child_download_get_acf_object_id() {
+function download_get_acf_object_id() {
 
 	if ( is_page() ) {
 		return get_the_ID();
@@ -38,7 +38,7 @@ function child_download_get_acf_object_id() {
 /**
  * 下載圖示（文件 + 下載箭頭）
  */
-function child_download_icon() {
+function download_icon() {
 	?>
 	<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
 		<path d="M6 2h8l5 5v13a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2z" fill="currentColor" opacity="0.15" />
@@ -51,10 +51,10 @@ function child_download_icon() {
 /**
  * 輸出資料下載區塊（掛在 footer 上方）
  */
-add_action( 'astra_footer_before', 'child_render_download_block' );
-function child_render_download_block() {
+add_action( 'astra_footer_before', 'render_download_block' );
+function render_download_block() {
 
-	$object_id = child_download_get_acf_object_id();
+	$object_id = download_get_acf_object_id();
 
 	if ( ! $object_id || ! get_field( 'show', $object_id ) ) {
 		return;
@@ -63,37 +63,37 @@ function child_render_download_block() {
 	$download_desc          = get_field( 'download-desc', 'option' );
 	$password_download_desc = get_field( 'password-download-desc', 'option' );
 	?>
-	<div class="child-download" data-object-id="<?php echo esc_attr( $object_id ); ?>">
+	<div class="download" data-object-id="<?php echo esc_attr( $object_id ); ?>">
 
-		<button type="button" class="child-download__trigger">
-			<span class="child-download__icon"><?php child_download_icon(); ?></span>
-			<span class="child-download__label">資料下載(PDF)</span>
+		<button type="button" class="download__trigger">
+			<span class="download__icon"><?php download_icon(); ?></span>
+			<span class="download__label">資料下載(PDF)</span>
 		</button>
 
 		<?php if ( $download_desc ) : ?>
-			<div class="child-download__notice"><?php echo wp_kses_post( $download_desc ); ?></div>
+			<div class="download__notice"><?php echo wp_kses_post( $download_desc ); ?></div>
 		<?php endif; ?>
 
-		<div class="child-download-modal" aria-hidden="true">
-			<div class="child-download-modal__overlay"></div>
+		<div class="download-modal" aria-hidden="true">
+			<div class="download-modal__overlay"></div>
 
-			<div class="child-download-modal__box" role="dialog" aria-modal="true" aria-labelledby="child-download-modal-title">
-				<button type="button" class="child-download-modal__close" aria-label="關閉">&times;</button>
+			<div class="download-modal__box" role="dialog" aria-modal="true" aria-labelledby="download-modal-title">
+				<button type="button" class="download-modal__close" aria-label="關閉">&times;</button>
 
-				<h3 id="child-download-modal-title" class="child-download-modal__title">資料下載</h3>
+				<h3 id="download-modal-title" class="download-modal__title">資料下載</h3>
 
-				<form class="child-download-modal__form" autocomplete="off">
-					<input type="password" name="download_password" class="child-download-modal__input" autocomplete="new-password" aria-label="下載密鑰">
+				<form class="download-modal__form" autocomplete="off">
+					<input type="password" name="download_password" class="download-modal__input" autocomplete="new-password" aria-label="下載密鑰">
 
 					<?php if ( $password_download_desc ) : ?>
-						<div class="child-download-modal__desc"><?php echo wp_kses_post( $password_download_desc ); ?></div>
+						<div class="download-modal__desc"><?php echo wp_kses_post( $password_download_desc ); ?></div>
 					<?php endif; ?>
 
-					<button type="submit" class="child-download-modal__submit" aria-label="下載">
-						<?php child_download_icon(); ?>
+					<button type="submit" class="download-modal__submit" aria-label="下載">
+						<?php download_icon(); ?>
 					</button>
 
-					<p class="child-download-modal__error" hidden></p>
+					<p class="download-modal__error" hidden></p>
 				</form>
 			</div>
 		</div>
@@ -104,10 +104,10 @@ function child_render_download_block() {
 /**
  * 載入下載功能專用的 CSS / JS（僅在區塊會顯示的頁面載入）
  */
-add_action( 'wp_enqueue_scripts', 'child_download_enqueue_assets' );
-function child_download_enqueue_assets() {
+add_action( 'wp_enqueue_scripts', 'download_enqueue_assets' );
+function download_enqueue_assets() {
 
-	$object_id = child_download_get_acf_object_id();
+	$object_id = download_get_acf_object_id();
 
 	if ( ! $object_id || ! get_field( 'show', $object_id ) ) {
 		return;
@@ -123,18 +123,18 @@ function child_download_enqueue_assets() {
 
 	wp_localize_script( 'astra-child-download-modal', 'ChildDownload', [
 		'ajax_url' => admin_url( 'admin-ajax.php' ),
-		'nonce'    => wp_create_nonce( 'child_download_nonce' ),
+		'nonce'    => wp_create_nonce( 'download_nonce' ),
 	] );
 }
 
 /**
  * AJAX：驗證下載密鑰，成功則回傳檔案網址
  */
-add_action( 'wp_ajax_child_verify_download_password', 'child_verify_download_password' );
-add_action( 'wp_ajax_nopriv_child_verify_download_password', 'child_verify_download_password' );
-function child_verify_download_password() {
+add_action( 'wp_ajax_verify_download_password', 'verify_download_password' );
+add_action( 'wp_ajax_nopriv_verify_download_password', 'verify_download_password' );
+function verify_download_password() {
 
-	check_ajax_referer( 'child_download_nonce', 'nonce' );
+	check_ajax_referer( 'download_nonce', 'nonce' );
 
 	$object_id = isset( $_POST['object_id'] ) ? sanitize_text_field( wp_unslash( $_POST['object_id'] ) ) : '';
 	$password  = isset( $_POST['password'] ) ? (string) wp_unslash( $_POST['password'] ) : '';
